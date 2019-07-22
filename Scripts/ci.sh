32ac1c45d9ff699a3417dab1fd27d16dea2ab15f
Dan Federman <dfed@me.com>
2019-07-21 21:17:31 -0700

    Add CI

---
+++ Scripts/ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+set -ex
+
+if [ $ACTION == "xcode" ]; then
+  if [ -n "$DESTINATION" ]; then
+    xcodebuild -UseModernBuildSystem=NO -workspace Floatation.xcworkspace -scheme "$SCHEME" -sdk $SDK -destination "$DESTINATION" -configuration Debug -PBXBuildsContinueAfterErrors=0 $XCODE_ACTION
+  else
+    xcodebuild -UseModernBuildSystem=NO -workspace Floatation.xcworkspace -scheme "$SCHEME" -sdk $SDK -configuration Debug -PBXBuildsContinueAfterErrors=0 $XCODE_ACTION
+  fi
+fi
+
+if [ $ACTION == "pod-lint" ]; then
+  bundle exec pod lib lint --verbose --fail-fast --swift-version=$SWIFT_VERSION
+fi
